f74c0f96304382b453f69e66176f12e3d5e971b4
TommyZ7 <tktomo7260t@gmail.com>
2025-05-25 22:47:53 +0900

    ホームページに手の検出アプリの説明とリンクを追加

---
+++ src/app/kennkyu/page.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1 className="text-3xl font-bold text-center sm:text-left">
+          手の検出アプリテスト
+        </h1>
+        <p className="text-lg text-gray-700 text-center sm:text-left">
+          手の検出を行うWebアプリケーションです。カメラを使用して手の動きをリアルタイムで検出します。
+        </p>
+        <div className="flex flex-col gap-4">
+          <Link href="/kennkyu/test1" className="text-blue-600 hover:underline">
+            テスト1:　初期バージョン（昔作ったやつ）
+          </Link>
+          <Link href="/kennkyu/test2" className="text-blue-600 hover:underline">
+            テスト2: テスト1にゲーム的なものをつけたやつ
+          </Link>
+          <Link href="/kennkyu/test3" className="text-blue-600 hover:underline">
+            テスト3: mediapipeの新しいバージョンを使用したやつ
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
+}
--- src/app/kennkyu/test3/page.tsx
+++ src/app/kennkyu/test3/page.tsx
@@ -4,9 +4,9 @@ import App from "@/components/test3/App";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
         <App />
-      </div>
+      </main>
     </>
   );
 }
